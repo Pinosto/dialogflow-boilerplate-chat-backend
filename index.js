69e7313f42f68bba2c50bb9cc98f02b4572918ec
Pinosto <juho.pinosto@hotmail.com>
2021-03-14 23:17:00 +0200

    ensimmäinen committi

---
+++ index.js
@@ -0,0 +1,61 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const keys = require('./keys.js')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', function (req, res) {
+  res.send('boilerchat')
+})
+app.post('/', async function (req, res) {
+
+  const projectId = keys.project_id;
+  const sessionId = req.body.uuid;
+  const query = req.body.text;
+  const languageCode = 'en-US';
+  const credentials = {
+    client_email: keys.client_email,
+    private_key: keys.private_key,
+  };
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient({
+    projectId,
+    credentials,
+  });
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  try {
+    // Send request and log result
+    const responses = await sessionClient.detectIntent(request);
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    res.json(result.fulfillmentText)
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+app.listen(3003)
